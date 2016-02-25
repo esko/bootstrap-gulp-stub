a96007b3e1cd77a392adb00747b292460d716faf
Esko Pyyluoma <esko.pyyluoma@f-secure.com>
2016-02-25 17:00:51 +0200

    autoprefixer, sass precision fix

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,34 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var del = require('del');
+var gulp         = require('gulp');
+var sass         = require('gulp-sass');
+var del          = require('del');
+var postcss      = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+
+var browsers = [
+  "Android 2.3",
+  "Android >= 4",
+  "Chrome >= 20",
+  "Firefox >= 24",
+  "Explorer >= 8",
+  "iOS >= 6",
+  "Opera >= 12",
+  "Safari >= 6"
+];
 
 gulp.task('init', ['build'], function() {
   return gulp.src(['node_modules/bootstrap-sass/assets/fonts/**',
-                   'node_modules/bootstrap-sass/assets/javascripts/**'],
+                   'node_modules/bootstrap-sass/assets/javascripts/bootstrap.*'],
                    { base: 'node_modules/bootstrap-sass/assets'})
     .pipe(gulp.dest('build'));
 });
 
 gulp.task('sass', function() {
 	return gulp.src('src/scss/*.scss')
-		.pipe(sass({includePaths: ['node_modules/bootstrap-sass/assets/stylesheets']}))
+		.pipe(sass({
+      includePaths: ['node_modules/bootstrap-sass/assets/stylesheets'],
+      precision: 8
+    }))
+    .pipe(postcss([autoprefixer({ browsers: browsers })]))
 		.pipe(gulp.dest('build/css'));
 });
 
